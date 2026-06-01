618a162d7100e9820fda312b5972315eacd12914
SOORYADEV B <sooryadevb777@gmail.com>
2026-06-01 14:20:50 +0530

    Add files via upload

---
+++ adminportal/config/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', include('portal.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
